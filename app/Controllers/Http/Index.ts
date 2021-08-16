c3669bed823f070b0b1503a8ba0fafae6667752c
DESKTOP-FAS9NCD\afuyan <dioaffriza@gmail.com>
2021-08-16 23:44:59 +0700

    Annoying typescript warning by removing unused vars

--- app/Controllers/Http/Index.ts
+++ app/Controllers/Http/Index.ts
@@ -1,12 +1,10 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import View from '@ioc:Adonis/Core/View'
 import PersonInfProvider from 'App/Utils/PersonInfProvider'
 
 export default class Index {
   public async index(ctx: HttpContextContract) {
     var personData = new PersonInfProvider()
-
-    return View.render('layout', {
+    return ctx.view.render('layout', {
       person: personData.get()
     })
   }
